5a093bd5bc9255c9776928cd9c26224049202306
GiGaFHTW <if25b075@technikum-wien.at>
2026-06-09 10:35:47 +0200

    refactor: rewrite update_quality + helper functions, corrected test case 3 and 5 (2x procs at -1 not 0)

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -2,85 +2,85 @@ describe("Gilded Rose", function() {
 
   it("normal item degrades quality by 1", function() {
     items = [ new Item("Normal Item", 10, 20) ];
-    update_quality();
+    updateItems();
     expect(items[0].quality).toEqual(19);
   });
 
   it("normal item decreases sell in by 1", function() {
     items = [ new Item("Normal Item", 10, 20) ];
-    update_quality();
+    updateItems();
     expect(items[0].sell_in).toEqual(9);
   });
 
   it("normal item degrades in quality 2x after sell in date", function() {
-    items = [ new Item("Normal Item", 0, 10) ];
-    update_quality();
+    items = [ new Item("Normal Item", -1, 10) ];
+    updateItems();
     expect(items[0].quality).toEqual(8);
   });
 
   it("quality is never negative", function() {
     items = [ new Item("Normal Item", 10, 0) ];
-    update_quality();
+    updateItems();
     expect(items[0].quality).toEqual(0);
   });
 
   it("aged brie increases quality by 1", function() {
     items = [ new Item("Aged Brie", 10, 20) ];
-    update_quality();
+    updateItems();
     expect(items[0].quality).toEqual(21);
   });
 
   it("aged brie increases quality by 2x after sell in date", function() {
-    items = [ new Item("Aged Brie", 0, 20) ];
-    update_quality();
+    items = [ new Item("Aged Brie", -1, 20) ];
+    updateItems();
     expect(items[0].quality).toEqual(22);
   });
 
   it("the quality of an item is never more then 50", function() {
     items = [ new Item("Aged Brie", 0, 49) ];
-    update_quality();
+    updateItems();
     expect(items[0].quality).toEqual(50);
   });
 
   it("Sulfuras never decreases in Quality", function() {
     items = [ new Item("Sulfuras, Hand of Ragnaros", 5, 80) ];
-    update_quality();
+    updateItems();
     expect(items[0].quality).toEqual(80);
   });
 
   it("Sulfuras never has to be sold", function() {
     items = [ new Item("Sulfuras, Hand of Ragnaros", 5, 80) ];
-    update_quality();
+    updateItems();
     expect(items[0].sell_in).toEqual(5)
   });
 
   it("backstage pass increases quality by 1 when sell_in > 10", function() {
     items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) ];
-    update_quality();
+    updateItems();
     expect(items[0].quality).toEqual(21);
   });
 
   it("backstage pass increases quality by 2 when sell_in <= 10", function() {
     items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) ];
-    update_quality();
+    updateItems();
     expect(items[0].quality).toEqual(22);
   });
 
   it("backstage pass increases quality by 3 when sell_in <= 5", function() {
     items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20) ];
-    update_quality();
+    updateItems();
     expect(items[0].quality).toEqual(23);
   });
 
   it("backstage pass quality drops to 0 after concert", function() {
     items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20) ];
-    update_quality();
+    updateItems();
     expect(items[0].quality).toEqual(0);
   });
 
   it("conjured item degrades quality by 2", function() {
     items = [ new Item("Conjured Mana Cake", 10, 20) ];
-    update_quality();
+    updateItems();
     expect(items[0].quality).toEqual(18);
   });
 
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -13,55 +13,36 @@ items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
-function update_quality() {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-          if(items[i].name.startsWith('Conjured')) {
-            if(items[i].quality > 0) {
-              items[i].quality = items[i].quality - 1; 
-            }
-          }
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
+function updateQuality(itemIndex, qualityChange) {
+  items[itemIndex].quality += qualityChange;
+  if(items[itemIndex].quality < 0) items[itemIndex].quality = 0;
+  if(items[itemIndex].quality > 50) items[itemIndex].quality = 50;
+}
+
+function getTicketPriceChange(itemIndex) {
+  if(items[itemIndex].sell_in <= 0) return -items[itemIndex].quality;
+  else if(items[itemIndex].sell_in <= 5 ) return 3;
+  else if(items[itemIndex].sell_in <= 10) return 2;
+  return 1;
+}
+
+function updateItems() {
+  for(var i = 0; i < items.length; i++) {
+    let qualityChange = -1
+
+    if(items[i].name == 'Sulfuras, Hand of Ragnaros') continue;
+    else if (items[i].name == 'Aged Brie') qualityChange = 1;
+    else if (items[i].name.startsWith('Backstage passes')) qualityChange = getTicketPriceChange(i);
+    else if (items[i].name.startsWith('Conjured')) qualityChange = -2;
+
+  
+
+    if(items[i].sell_in < 0) {
+      qualityChange = qualityChange * 2;
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
-    }
+    
+    items[i].sell_in--;
+
+    updateQuality(i, qualityChange);
   }
 }
